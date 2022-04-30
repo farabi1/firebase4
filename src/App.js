466c7ee150e7c5c05ae60c4af64085ffd5450385
farabi1 <rashidfarabi@gmail.com>
2022-04-30 11:51:08 +0600

    deploy with Git

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Firebase 4 Git and Deployment</h1>
+     <h1>Firebase 4 Git and Deployment with git push</h1>
     </div>
   );
 }
